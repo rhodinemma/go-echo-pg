8e5cef9b45d2f71619c192eb86ad386047adcc53
rhodinemma <rhodinemma10@gmail.com>
2023-11-13 02:41:50 +0300

    feat: wire up the service

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,10 +6,13 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/rhodinemma/go-echo-pg/internal/database"
+	"github.com/rhodinemma/go-echo-pg/internal/models"
 )
 
 type Server interface {
-	start() error
+	Start() error
+	Readiness(ctx echo.Context) error
+	Liveness(ctx echo.Context) error
 }
 
 type EchoServer struct {
@@ -29,7 +32,7 @@ func NewEchoServer(db database.DatabaseClient) Server {
 
 func (s *EchoServer) Start() error {
 	if err := s.echo.Start(":8080"); err != nil && err != http.ErrServerClosed {
-		log.Fatal("server shutdown occurred: %s", err)
+		log.Fatalf("server shutdown occurred: %s", err)
 		return err
 	}
 
@@ -37,5 +40,19 @@ func (s *EchoServer) Start() error {
 }
 
 func (s *EchoServer) registerRoutes() {
+	s.echo.GET("/readiness", s.Readiness)
+	s.echo.GET("/liveness", s.Liveness)
+}
 
+func (s *EchoServer) Readiness(ctx echo.Context) error {
+	ready := s.DB.Ready()
+	if ready {
+		return ctx.JSON(http.StatusOK, models.Health{Status: "OK"})
+	}
+
+	return ctx.JSON(http.StatusInternalServerError, models.Health{Status: "Failure"})
+}
+
+func (s *EchoServer) Liveness(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, models.Health{Status: "OK"})
 }
--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"github.com/rhodinemma/go-echo-pg/internal/database"
+	"github.com/rhodinemma/go-echo-pg/internal/server"
+)
+
 func main() {
+	db, err := database.NewDatabaseClient()
+	if err != nil {
+		log.Fatalf("failed to initialize Database client: %s", err)
+	}
+
+	srv := server.NewEchoServer(db)
+	if err := srv.Start(); err != nil {
+		log.Fatal(err.Error())
+	}
+
 }
